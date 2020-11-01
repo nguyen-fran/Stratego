3f52f90d55d9096d9385afbb38526b902195e730
g-marshak <marshak22@up.edu>
2020-11-01 14:22:13 -0800

    moved move and swap logic to the local game (needs testing)

--- app/src/main/java/edu/up/cs301/Stratego/StrategoMoveAction.java
+++ app/src/main/java/edu/up/cs301/Stratego/StrategoMoveAction.java
@@ -4,13 +4,32 @@ import edu.up.cs301.game.GameFramework.GamePlayer;
 import edu.up.cs301.game.GameFramework.actionMessage.GameAction;
 
 public class StrategoMoveAction extends GameAction {
+    private BoardSquare squareSrc;
+    private BoardSquare squareDest;
 
     /**
      * constructor for GameAction
      *
      * @param player the player who created the action
      */
-    public StrategoMoveAction(GamePlayer player) {
+    public StrategoMoveAction(GamePlayer player, BoardSquare squareSrc, BoardSquare squareDest) {
         super(player);
+        this.squareSrc = squareSrc;
+        this.squareDest = squareDest;
+    }
+
+    //getters and setters
+    public BoardSquare getSquareSrc() {
+        return squareSrc;
+    }
+    public BoardSquare getSquareDest() {
+        return squareDest;
+    }
+
+    public void setSquareSrc(BoardSquare squareSrc) {
+        this.squareSrc = squareSrc;
+    }
+    public void setSquareDest(BoardSquare squareDest) {
+        this.squareDest = squareDest;
     }
 }
--- app/src/main/java/edu/up/cs301/Stratego/StrategoSwapAction.java
+++ app/src/main/java/edu/up/cs301/Stratego/StrategoSwapAction.java
@@ -4,13 +4,33 @@ import edu.up.cs301.game.GameFramework.GamePlayer;
 import edu.up.cs301.game.GameFramework.actionMessage.GameAction;
 
 public class StrategoSwapAction extends GameAction {
+    private BoardSquare squareSrc;
+    private BoardSquare squareDest;
 
     /**
      * constructor for GameAction
      *
      * @param player the player who created the action
      */
-    public StrategoSwapAction(GamePlayer player) {
+    public StrategoSwapAction(GamePlayer player, BoardSquare squareSrc, BoardSquare squareDest) {
         super(player);
+        this.squareSrc = squareSrc;
+        this.squareDest = squareDest;
+    }
+
+    //getters and setters
+
+    public BoardSquare getSquareSrc() {
+        return squareSrc;
+    }
+    public BoardSquare getSquareDest() {
+        return squareDest;
+    }
+
+    public void setSquareSrc(BoardSquare squareSrc) {
+        this.squareSrc = squareSrc;
+    }
+    public void setSquareDest(BoardSquare squareDest) {
+        this.squareDest = squareDest;
     }
 }
