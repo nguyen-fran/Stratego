0ebfb5e794ada14b17bb67c8818594c40f49368f
g-marshak <marshak22@up.edu>
2020-11-07 14:51:07 -0800

    recieveInfo can update graveyards (still needs to update gameboard)

--- app/src/main/java/edu/up/cs301/Stratego/StrategoHumanPlayer.java
+++ app/src/main/java/edu/up/cs301/Stratego/StrategoHumanPlayer.java
@@ -1,5 +1,7 @@
 package edu.up.cs301.Stratego;
 
+import android.graphics.Color;
+import android.util.Log;
 import android.view.View;
 
 import edu.up.cs301.R;
@@ -9,6 +11,9 @@ import edu.up.cs301.game.GameFramework.infoMessage.GameInfo;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class StrategoHumanPlayer extends GameHumanPlayer implements OnClickListener {
 
@@ -17,6 +22,11 @@ public class StrategoHumanPlayer extends GameHumanPlayer implements OnClickListe
     private Button move;
 
     private ViewGroup gameBoardGrid;
+    private GridLayout playerGY;
+    private GridLayout oppGY;
+
+    public static final int BLUE = 0;
+    public static final int RED = 1;
 
     /**
      * constructor
@@ -34,6 +44,59 @@ public class StrategoHumanPlayer extends GameHumanPlayer implements OnClickListe
 
     @Override
     public void receiveInfo(GameInfo info) {
+        Log.i("human player", "recieveInfo called");
+
+        if(!(info instanceof StrategoGameState)){
+            flash(Color.RED, 10);
+            return;
+        }
+
+        /**
+         External Citation
+         Date: 7 Nov 2020
+         Problem: needed a way to loop though layouts to get all the elements
+         Resource: https://stackoverflow.com/questions/19523860/iterate-through-all-objects-in-gridview
+         Solution: used this as an example
+         */
+
+
+        //setting up player graveyard with a loop
+        for (int i = 0; i < 11; i++){
+            TextView GY = (TextView)playerGY.getChildAt(i + 11);
+            //setting text to whatever value is in graveyard array at that coord
+            //might need to adjust +- 1 depending to avoid out of bounds errors
+            GY.setText(((StrategoGameState) info).getBlueGY()[i]);
+        }
+
+        //setting up computer graveyard with a loop
+        for (int i = 0; i < 11; i++){
+            TextView GY = (TextView)oppGY.getChildAt(i + 11);
+            //setting text to whatever value is in graveyard array at that coord
+            //might need to adjust +- 1 depending to avoid out of bounds errors
+            GY.setText(((StrategoGameState) info).getRedGY()[i]);
+        }
+
+        //double for loop to update game board from game state
+        for (int i = 0; i < 10; i++){
+            for (int j = 0; j < 10; j++){
+                //using this to be able to get from the board grid at the correct place
+                int gridCoord = (i*10) + j;
+                ImageButton square = (ImageButton)gameBoardGrid.getChildAt(gridCoord);
+
+                //TODO: find out how to set text on imagebuttons or get images in here
+                //might need to redo this based on filenames when switching to images
+
+                //set text color based on team
+                if(((StrategoGameState) info).getBoardSquares()[i][j].getPiece().getTeam() == BLUE){
+                    //set text to blue
+                }else{
+                    //set text to red
+                }
+
+                //set text based on rank
+
+            }
+        }
 
     }
 
@@ -46,6 +109,9 @@ public class StrategoHumanPlayer extends GameHumanPlayer implements OnClickListe
         for (int i = 0; i < 100; i++) {
             gameBoardGrid.addView(new Button(myActivity));
         }
+
+        this.playerGY = activity.findViewById(R.id.blueGY);
+        this.oppGY = activity.findViewById(R.id.redGY);
 
 //        swap = myActivity.findViewById(R.id.swap);
 //        move = myActivity.findViewById(R.id.move);
